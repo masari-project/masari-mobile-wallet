6edeed3ab1538f5c60da4c5bee749cc6814142e3
cryptochangements34 <bW9uZXJv@gmail.com>
2019-08-14 20:20:07 -0400

    use submodule

--- scripts/beforeBuild.sh
+++ scripts/beforeBuild.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+if [ ! -d www ]
+then
 mkdir www
-cp -r src/. www
+fi
+
+cd masari-webwallet
+npm install
+nodejs ./node_modules/typescript/bin/tsc --project tsconfig.prod.json
+nodejs build.js
+cd ../
+
+cp -r masari-webwallet/src/. www
 cp -r src_specific/. www
